18de3ecd24ac16f3f6f0ddbad2fa7e3aacf32a15
kangdongkil <kbk3418@naver.com>
2014-12-04 13:58:34 +0900

    s

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var system;
+
+function setup() {
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(width/2, 50));
+}
+
+function draw() {
+  img = loadImage("bunsu.png");
+  background(51);
+  system.addParticle();
+  system.run();
+  drawBunsu();
+
+  
+}
+function mouseClicked(){
+  if(f===0){
+        f=1;
+    }else if(f===1){
+        f=0;
+    }
+  
+}
+function drawBunsu(){
+  image(img,img.width/2,img.height/2);
+}
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.get();
+  this.lifespan = 255.0;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  if (this.lifespan < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var ParticleSystem = function(position) {
+  this.origin = position.get();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length-1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
