7f93c909739e861609c725e53ac52722787d587a
kangdongkil <kbk3418@naver.com>
2014-12-04 16:31:13 +0900

    d

--- sketch.js
+++ sketch.js
@@ -5,6 +5,12 @@ var img3;
 var dir1=0;
 var dir2=0;
 var f = 0;
+var r;
+
+// Angle and angular velocity, accleration
+var theta;
+var theta_vel;
+var theta_acc;
 
 function setup() {
   createCanvas(720, 400);
@@ -12,6 +18,10 @@ function setup() {
   img2 = loadImage("s.png")
   img3 = loadImage("s2.png")
   system = new ParticleSystem(createVector(width/2, 50));
+  r = height * 0.45;
+  theta = 0;
+  theta_vel = 0;
+  theta_acc = 0.0001;
  
 }
 
@@ -22,7 +32,29 @@ function draw() {
   system.run();
   drawSa();
   drawSaram();
+  drawBang();
+}
+function drawBang() {
   
+  background(0);
+  
+  // Translate the origin point to the center of the screen
+  translate(width/2, height/2);
+  
+  // Convert polar to cartesian
+  var x = r * cos(theta);
+  var y = r * sin(theta);
+  
+  // Draw the ellipse at the cartesian coordinate
+  ellipseMode(CENTER);
+  noStroke();
+  fill(200);
+  ellipse(x, y, 32, 32);
+  
+  // Apply acceleration and velocity to angle 
+  // (r remains static in this example)
+  theta_vel += theta_acc;
+  theta += theta_vel;
 }
 
 function mouseClicked(){
