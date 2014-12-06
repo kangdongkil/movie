df221c1077006e0a1f7df8187d454b0414fa3f7a
kangdongkil <kbk3418@naver.com>
2014-12-06 21:05:25 +0900

    a

--- sketch.js
+++ sketch.js
@@ -8,10 +8,17 @@ var dir2=0;
 var f = 0;
 var r;
 var bug; 
+// A sound file object
+var song;
 
+function preload() {
+  // Load a sound file
+  song = loadSound('a.mp3');
+}
 
 function setup() {
   createCanvas(720, 400);
+  song.loop();
   img = loadImage("bunsu.PNG");
   img2 = loadImage("s.png");
   img3 = loadImage("s2.png");
@@ -30,6 +37,25 @@ function draw() {
   drawSa();
   drawSaram();
   drawBang();
+
+  // Set the volume to a range between 0 and 1.0
+  var volume = map(mouseX, 0, width, 0, 1);
+  volume = constrain(volume, 0, 1);
+  song.amp(volume);
+
+  // Set the rate to a range between 0.1 and 4
+  // Changing the rate alters the pitch
+  var speed = map(mouseY, 0.1, height, 0, 2);
+  speed = constrain(speed, 0.01, 4);
+  song.rate(speed);
+
+  // Draw some circles to show what is going on
+  stroke(0);
+  fill(51, 100);
+  ellipse(mouseX, 100, 48, 48);
+  stroke(0);
+  fill(51, 100);
+  ellipse(100, mouseY, 48, 48);
 
 }
 
