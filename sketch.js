5233a9f9ce238bc75a7ecd801012fad320397c9c
kangdongkil <kbk3418@naver.com>
2014-12-06 21:16:17 +0900

    d

--- sketch.js
+++ sketch.js
@@ -39,9 +39,7 @@ function draw() {
   drawBang();
 
   // Set the volume to a range between 0 and 1.0
-  var volume = map(mouseX, 0, width, 0, 1);
-  volume = constrain(volume, 0, 1);
-  song.amp(volume);
+ 
 
   // Set the rate to a range between 0.1 and 4
   // Changing the rate alters the pitch
